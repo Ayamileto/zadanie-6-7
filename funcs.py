026a77ca33ca5ee1d36cf9604a4e58c1b2d7f60b
Балахонцева Анастасия <ayamileto@gmail.com>
2023-05-14 07:59:29 +0300

    add second function

--- funcs.py
+++ funcs.py
@@ -1,3 +1,8 @@
 def sum_numbers(first_num, second_num):
     sum_ = int(first_num + second_num)
     return sum_
+
+
+def minus_numbers(first_numb, second_numb):
+    minus = int(first_numb - second_numb)
+    return minus
